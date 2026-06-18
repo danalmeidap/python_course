24a4e2a02acf4a2e1f4a6393ecb6c799e656fc01
danalmeidap <dbatistadealmeida23@gmail.com>
2026-06-18 20:17:57 -0300

    refactor: remove SQLite-specific connect_args for PostgreSQL compatibility

--- app/settings/database.py
+++ app/settings/database.py
@@ -5,10 +5,7 @@ from app.settings.settings import Settings
 settings = Settings()
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(DATABASE_URL,
-                       connect_args={"check_same_thread": False}
-        )
-
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
