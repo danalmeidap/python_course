8b8935a016a5109b8549030c6a766b7ae15b60b0
danalmeidap <dbatistadealmeida23@gmail.com>
2026-06-20 13:59:17 -0300

    fix: strip whitespace from DATABASE_URL

--- app/settings/database.py
+++ app/settings/database.py
@@ -3,11 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.settings.settings import Settings
 
 settings = Settings()
-DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = settings.DATABASE_URL.strip()
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+DATABASE_URL = settings.DATABASE_URL
+print(f"DATABASE_URL recebida: {DATABASE_URL[:30]}...")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
